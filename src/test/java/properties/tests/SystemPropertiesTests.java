cdc986e591d12052f5f6336147f16ae94871b78b
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-25 17:54:29 +0300

    Added hello test

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -41,5 +43,18 @@ public class SystemPropertiesTests {
 
         System.out.println(browser);
         // gradle property_test -Dbrowser=opera
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message);
+        // gradle hello_test
+        // Hello, default student!
+
+        // gradle hello_test -Dname='John Smith'
     }
 }
