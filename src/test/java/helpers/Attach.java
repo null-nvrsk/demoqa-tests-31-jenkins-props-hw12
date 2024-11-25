66fc8a2ffd469860539f7997139b22d7228d190a
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-26 00:06:21 +0300

    Fix attach file

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,7 +44,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String videoUrl = "https://" + System.getProperty("selenoidUrl", "selenoid.autotests.cloud")
+                + "/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
